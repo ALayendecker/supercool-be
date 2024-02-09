95323b4f80ca69c7650bc54e4e8daa698f1b9630
ALayendecker <andrewlayendecker@gmail.com>
2024-02-09 10:06:54 -0600

    splitting to be/fe

---
+++ schemas/resolvers.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    users: async (parent, args, context, __)=> {
+      const users = await User.find()
+      return users
+    }
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  }
+};
+
+module.exports = resolvers;
